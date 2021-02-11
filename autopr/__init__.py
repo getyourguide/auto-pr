c2740c0a42481a340fb76aef2d05b12ba40cd8b5
Malachi Soord <inverse.chi@gmail.com>
2021-02-11 15:50:04 +0100

    Fix tests

--- autopr/__init__.py
+++ autopr/__init__.py
@@ -140,7 +140,7 @@ def test():
     default=None,
     help="Delay in seconds between pushing changes to repositories",
 )
-def run(delay: Optional[float]):
+def run(push_delay: Optional[float]):
     """ Perform execution of update and create pull requests. """
     cfg = workdir.read_config(WORKDIR)
     db = workdir.read_database(WORKDIR)
@@ -171,9 +171,9 @@ def run(delay: Optional[float]):
         workdir.write_database(WORKDIR, db)
         click.secho(f"Done updating repository '{repository.name}'")
 
-        if updated and delay is not None:
-            click.secho(f"Sleeping for {delay} seconds...")
-            time.sleep(delay)
+        if updated and push_delay is not None:
+            click.secho(f"Sleeping for {push_delay} seconds...")
+            time.sleep(push_delay)
 
 
 @cli.command()
