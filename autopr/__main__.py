ff8a80759887f0737f1a6d731dacecec860f01b6
Malachi Soord <inverse.chi@gmail.com>
2021-03-22 10:05:44 +0100

    Fix package (#20)
    * Fix package
    
    * update changelog

--- autopr/__main__.py
+++ autopr/__main__.py
@@ -1,17 +1,4 @@
-from autopr import cli
-from autopr.util import CliException, is_debug, error
-
-
-def main():
-    try:
-        cli(prog_name="auto-pr")
-    except CliException as e:
-        error(f"Error: {e}")
-        if is_debug():
-            raise e
-        else:
-            exit(1)
-
+from autopr import main
 
 if __name__ == "__main__":
     main()
